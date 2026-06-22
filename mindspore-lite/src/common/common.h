96e7282774b3ad2dbf752e9801411a16cc1564d4
linhai <lihaolin11@huawei.com>
2026-06-22 16:48:57 +0800

    max漏洞修复

--- mindspore-lite/src/common/common.h
+++ mindspore-lite/src/common/common.h
@@ -46,6 +46,10 @@ static constexpr int kNHWCDimNumber = 4;
 
 static constexpr int TENSOR_MAX_REFCOUNT = 999;
 
+// FlatBuffers verifier security limits
+static constexpr int FLATBUFFERS_MAX_DEPTH = 64;        // Prevent recursive stack overflow
+static constexpr int FLATBUFFERS_MAX_TABLES = 1000000;  // Prevent CPU exhaustion
+
 // quantization relative
 static const char QUANTIZED_UINT8[] = "QUANTIZED_UINT8";
 static const char QUANTIZED_INT8[] = "QUANTIZED_INT8";
